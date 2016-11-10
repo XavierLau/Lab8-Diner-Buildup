ac876c9542b62dd3546343fb3f673c2b0f5cf274
Xavier Lau <xavierlau1@gmail.com>
2016-11-10 15:14:14 -0800

    Update form view to show submit and cancel buttons

--- application/views/mtce-edit.php
+++ application/views/mtce-edit.php
@@ -1,9 +1,10 @@
 <h2>Menu Maintenance - Editing</h2>
-<form action="#" method="post">
+<form action="/curd/save" method="post">
     {fid}
     {fname}
     {fdescription}
     {fprice}
     {fpicture}
     {fcategory}
+    {zsubmit} <a class="btn btn-default" role="buton" href="/crud/cancel">Cancel</a>
 </form>
