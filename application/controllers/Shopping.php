70e5866e7eb53688f6a35ed315e8f7912eccd429
Jeremy Yang <yy25036542@gmail.com>
2016-11-17 14:45:28 -0800

    Completed part 1 to 8 of the lab, and removed the annoying footer. Signed-off-by:Jeremy Yang <yy25036542@gmail.com>

--- application/controllers/Shopping.php
+++ application/controllers/Shopping.php
@@ -6,27 +6,66 @@ class Shopping extends Application
 {
 
 	function __construct() {
-		parent::__construct();
+            parent::__construct();
 	}
 	
-	public function index()
-	{
-        $stuff = file_get_contents('../data/receipt.md');
-        $this->data['receipt'] = $this->parsedown->parse($stuff);
-		$this->data['content'] = '';
-		
-		// pictorial menu
-		$count = 1;
-		foreach ($this->categories->all() as $category) {
-			$chunk = 'category' . $count; 
-			$this->data[$chunk] = $this->parser->parse('category-shop',$category,true);
-			foreach($this->menu->all() as $menuitem) {
-				if ($menuitem->category != $category->id) continue;
-				$this->data[$chunk] .= $this->parser->parse('menuitem-shop',$menuitem,true);
-			}
-			$count++;
-		}
-		$this->render('template-shopping'); 
+	public function index()	{
+            if($this->session->has_userdata('order')) {
+                $this->keep_shopping();
+            } else {
+                $this->summarize();
+            }
 	}
+        
+        public function summarize() {
+            $this->data['pagebody'] = 'summary';
+            $this->render('template');  // use the default template
+        }
+        
+        public function keep_shopping() {
+            $order = new Order($this->session->userdata('order'));
+            $stuff = $order->receipt();
+            $this->data['receipt'] = $this->parsedown->parse($stuff);
+            $this->data['content'] = '';
+            
+            // pictorial menu
+            $count = 1;
+            foreach($this->categories->all() as $category) {
+                $chunk = 'category' . $count; 
+                $this->data[$chunk] = $this->parser->parse('category-shop',$category,true);
+                foreach($this->menu->all() as $menuitem) {
+                        if ($menuitem->category != $category->id) continue;
+                        $this->data[$chunk] .= $this->parser->parse('menuitem-shop',$menuitem,true);
+                }
+                $count++;
+            }
+            $this->render('template-shopping'); 
+        }
+        
+        public function neworder() {
+            // create a new order if needed
+            if(! $this->session->has_userdata('order')) {
+                $order = new Order();
+                $this->session->set_userdata('order', (array)$order);
+            }
 
+            $this->keep_shopping();
+        }
+        
+        public function add($what) {
+            $order = new Order($this->session->userdata('order'));
+            $order->additem($what);
+            $this->session->set_userdata('order',(array)$order);
+            $this->keep_shopping();
+            redirect('/shopping');
+        }
+        
+        public function cancel() {
+            // Drop any order in progress
+            if ($this->session->has_userdata('order')) {
+                $this->session->unset_userdata('order');
+            }
+
+            $this->index();
+        }
 }
